5d94e8d5a0d409c89a4a0b1f4723bf1e267d45f4
rodrigoruan <rodrigopython16@gmail.com>
2021-11-01 13:08:35 -0300

    setup: configured routes with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './Pages/Login';
 
 function App() {
-  return <div />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
